1a5376e5e0c039e3f824de8a3e9e278bb516ec39
Anish Bhatta <anish@Anishs-MacBook-Pro.local>
2021-02-09 23:30:40 +0530

    First Commit

--- src/test/java/com/example/multitenancy/SampleMultienancyApplicationTests.java
+++ src/test/java/com/example/multitenancy/SampleMultienancyApplicationTests.java
@@ -1,18 +1,10 @@
 package com.example.multitenancy;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 @SpringBootTest
 public class SampleMultienancyApplicationTests {
-
-	//@Test
-	//@EnabledIf("#{T(com.example.multitenancy.config.Constants).TEST == true}")
-	@EnabledIf("#{T(com.example.multitenancy.config.Constants).TEST == true}")
-	@Test
-	public void theMeasurementSampleScreenFromPickConfirm() throws InterruptedException {
-	       System.out.println("Hi");
-	}
+	
+	
 
 }
